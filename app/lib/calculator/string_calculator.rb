9d682d1d2834e65a4dfed8fbebac555cd6e76d8a
Harshdeep <harshsingh9613@gmail.com>
2025-04-16 13:00:16 +0530

    Support custom single-character delimiter

--- app/lib/calculator/string_calculator.rb
+++ app/lib/calculator/string_calculator.rb
@@ -4,8 +4,13 @@ module Calculator
     def self.add(numbers)
       return 0 if numbers.empty?
     
-      numbers = numbers.gsub("\n", ",")
-      numbers.split(",").map(&:to_i).sum
+      delimiter = ",|\n"
+      if numbers.start_with?("//")
+        delimiter = Regexp.escape(numbers[2])
+        numbers = numbers.split("\n", 2).last
+      end
+    
+      numbers.split(/#{delimiter}/).map(&:to_i).sum
     end
 
   end
