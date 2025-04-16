0e1feab4125aed46faef4b20b3954b8d6ca37671
Harshdeep <harshsingh9613@gmail.com>
2025-04-16 12:47:55 +0530

    Add spec for single number

--- spec/lib/calculator/string_calculator_spec.rb
+++ spec/lib/calculator/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Calculator::StringCalculator do
     it "returns 0 for an empty string" do
       expect(described_class.add("")).to eq(0)
     end
+
+    it "returns the number itself for a single number" do
+      expect(described_class.add("1")).to eq(1)
+    end
   end
 end
